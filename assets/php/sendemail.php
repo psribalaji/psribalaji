a9c46c8c367a9f2f2e6ee628f839c6d413e89670
psribalaji <psribalaji12@gmail.com>
2017-12-07 21:44:40 +0530

    php modified

--- assets/php/sendemail.php
+++ assets/php/sendemail.php
@@ -6,7 +6,7 @@
 	$message = @trim(stripslashes($_POST['message'])); 
 
 	$email_from = $email;
-	$email_to = 'yourmail@gmail.com'; //replace with your email
+	$email_to = 'psribalaji12@gmail.com'; //replace with your email
 
 	$body = 'Name: ' . $name . "\n\n" . 'Email: ' . $email . "\n\n" . 'Subject: ' . $subject . "\n\n" . 'Message: ' . $message;
 
@@ -18,5 +18,5 @@
 <html lang="en-US">
 <head>
 	<script>alert("Thank you for contact us. As early as possible  we will contact you.");</script>
-	<meta HTTP-EQUIV="REFRESH" content="0; url=http://trendytheme.net/demo/iamx/v/"> 
+	<meta HTTP-EQUIV="REFRESH" content="0; url=https://psribalaji.github.io/psribalaji/"> 
 </head>
